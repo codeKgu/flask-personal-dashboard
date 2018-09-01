eeb1228c00b0c31a49ba3a452d751971b5605333
Ken Gu <ken.gu@ticketmaster.com>
2018-08-31 23:46:30 -0700

    Initial commit

--- app/base/forms.py
+++ app/base/forms.py
@@ -1,15 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, PasswordField
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Length
 
 ## login and registration
 
 
 class LoginForm(FlaskForm):
-    username = TextField('Username', id='username_login')
+    username = StringField('Username', id='username_login', validators=[DataRequired(),
+                                                                        Length( min=5, max=20)])
     password = PasswordField('Password', id='pwd_login')
 
 
 class CreateAccountForm(FlaskForm):
-    username = TextField('Username', id='username_create')
-    email = TextField('Email')
+    username = StringField('Username', id='username_create')
+    email = StringField('Email')
     password = PasswordField('Password', id='pwd_create')
