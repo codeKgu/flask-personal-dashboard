c7db71ce3638a30317f840f9153a88e569ef6353
Ken Gu <ken.qgu@gmail.com>
2018-09-25 19:51:34 -0700

    bind to 0.0.0.0

--- gentelella.py
+++ gentelella.py
@@ -1,9 +1,10 @@
 from app import create_app, db
 from flask_migrate import Migrate
-
+import os
 app = create_app()
 Migrate(app, db)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=port)
